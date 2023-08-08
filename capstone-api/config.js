e2c1bbbd1c1172ab075dcd193722e49818a71536
Jordan Sarkodie <jsarkodie@jsarkod-ltmvcgr.internal.salesforce.com>
2023-08-08 15:54:02 -0700

    added a commented out line for localhost and a line for deployment in the config file.

--- capstone-api/config.js
+++ capstone-api/config.js
@@ -16,7 +16,8 @@ function getDatabaseUri() {
     const dbProdName = process.env.DATABASE_NAME || "capstone"
     const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
   
-    return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+    // return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+    return render || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
   }
   const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13
 
